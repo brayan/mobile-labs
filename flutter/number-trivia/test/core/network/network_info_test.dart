4b5ece0748ccc8b602ca2052b636dd8d7e843617
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2020-06-30 09:30:11 -0300

    Add implementation and tests to NetworkInfo

---
+++ flutter/number-trivia/test/core/network/network_info_test.dart
@@ -0,0 +1,30 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:numbertrivia/core/network/network_info.dart';
+
+class MockDataConnectionChecker extends Mock implements DataConnectionChecker {}
+
+void main() {
+  NetworkInfoImpl networkInfo;
+  MockDataConnectionChecker mockDataConnectionChecker;
+
+  setUp(() {
+    mockDataConnectionChecker = MockDataConnectionChecker();
+    networkInfo = NetworkInfoImpl(mockDataConnectionChecker);
+  });
+
+  group('isConnected', () {
+    test('should forward the call to DataConnectionChecker.hasConnection', () async {
+      final hasConnectionFuture = Future.value(true);
+      when(mockDataConnectionChecker.hasConnection).thenAnswer((_) => hasConnectionFuture);
+
+      final result = networkInfo.isConnected;
+
+      verify(mockDataConnectionChecker.hasConnection);
+      expect(result, hasConnectionFuture);
+
+    });
+  });
+
+}
