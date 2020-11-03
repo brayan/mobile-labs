92213436cfae529c005129ca35ab8eb006e42c43
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2020-11-03 08:37:32 -0300

    Handle contact-list loading status

--- flutter/bytebank_sqlite/lib/screens/contacts_list.dart
+++ flutter/bytebank_sqlite/lib/screens/contacts_list.dart
@@ -1,3 +1,5 @@
+import 'package:bytebank_sqlite/database/app_database.dart';
+import 'package:bytebank_sqlite/models/contact.dart';
 import 'package:bytebank_sqlite/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
@@ -5,39 +7,79 @@ class ContactsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Contacts'),
-      ),
-      body: ListView(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              title: Text(
-                'Brayan',
-                style: TextStyle(
-                  fontSize: 24.0,
+      appBar: AppBar(title: Text('Contacts')),
+      body: FutureBuilder<List<Contact>>(
+        initialData: List(),
+        // future: Future.delayed(Duration(seconds: 1)).then((value) => findAll()),
+        future: findAll(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[CircularProgressIndicator(), Text("Loading")],
                 ),
-              ),
-              subtitle: Text(
-                '1000',
-                style: TextStyle(
-                  fontSize: 16.0,
-                ),
-              ),
-            ),
-          )
-        ],
+              );
+              break;
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              final List<Contact> contacts = snapshot.data;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final contact = contacts[index];
+                  return _ContactItem(contact);
+                },
+                itemCount: contacts.length,
+              );
+              break;
+          }
+
+          return Text("Unknown error");
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => ContactForm(),
-            ),
-          ).then((newContact) => debugPrint(newContact.toString()));
+          _onContactFormPressed(context);
         },
         child: Icon(
           Icons.add,
+        ),
+      ),
+    );
+  }
+
+  void _onContactFormPressed(BuildContext context) {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => ContactForm()))
+        .then((newContact) => debugPrint(newContact.toString()));
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+  final Contact contact;
+
+  const _ContactItem(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          contact.name,
+          style: TextStyle(
+            fontSize: 24.0,
+          ),
+        ),
+        subtitle: Text(
+          contact.accountNumber.toString(),
+          style: TextStyle(
+            fontSize: 16.0,
+          ),
         ),
       ),
     );
