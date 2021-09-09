174e843210ffc055d67375852086c4eb15a5a629
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2021-09-09 08:31:13 -0300

    Add core UI components

--- android/udemy-jetpack-compose/app/src/main/java/br/com/sailboat/udemy_jetpack_compose/MainActivity.kt
+++ android/udemy-jetpack-compose/app/src/main/java/br/com/sailboat/udemy_jetpack_compose/MainActivity.kt
@@ -1,38 +1,100 @@
 package br.com.sailboat.udemy_jetpack_compose
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.sailboat.udemy_jetpack_compose.ui.theme.UdemyjetpackcomposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            UdemyjetpackcomposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            CoreUiCompose()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CoreUiCompose() {
+    Surface(
+        color = Color.DarkGray,
+        modifier = Modifier.fillMaxSize(), // match height and width
+    ) {
+        Surface(
+            color = Color.Blue,
+            modifier = Modifier.wrapContentSize(align = Alignment.TopEnd),
+        ) {
+            Text(
+                text = "Wrapped content, height and width",
+                color = Color.White,
+                modifier = Modifier.wrapContentSize(),
+                style = MaterialTheme.typography.body1
+            )
+        }
+    }
+}
+
+@Composable
+fun BasicMainActivityCompose(context: Context) {
+    UdemyjetpackcomposeTheme {
+        Surface(color = MaterialTheme.colors.background) {
+            GreetingText("Android")
+            GreetingButton("Android", onClick = {
+                Toast.makeText(context, "Hey!", Toast.LENGTH_SHORT).show()
+            })
+        }
+    }
+}
+
+@Composable
+fun GreetingText(name: String) {
+    Text(
+        text = "Hello, $name!",
+        modifier = Modifier
+//            .fillMaxHeight()
+            .fillMaxWidth()
+//            .wrapContentWidth()
+            .wrapContentHeight()
+//            .width(80.dp)
+//            .height(240.dp)
+            .clickable { }
+//            .padding(all = 12.dp)
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+//            style = TextStyle(
+//                color = Color.White,
+//                fontWeight = FontWeight.SemiBold,
+//                fontSize = 18.sp
+//            )
+        style = MaterialTheme.typography.h5,
+        fontWeight = FontWeight.SemiBold
+    )
+}
+
+@Composable
+fun GreetingButton(name: String, onClick: () -> Unit) {
+    Button(onClick = onClick) {
+        GreetingText(name = "Click-me, $name!!")
+        GreetingText(name = "Hello!")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreviewMainActivity() {
-    UdemyjetpackcomposeTheme {
-        Greeting("Android")
-    }
+    CoreUiCompose()
 }
