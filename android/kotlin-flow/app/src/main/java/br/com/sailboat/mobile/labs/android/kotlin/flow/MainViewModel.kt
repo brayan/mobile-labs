1e329177998dd342054e920f9f4ee2f941c3b1b3
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2022-06-15 09:05:03 -0300

    Add operators

--- android/kotlin-flow/app/src/main/java/br/com/sailboat/mobile/labs/android/kotlin/flow/MainViewModel.kt
+++ android/kotlin-flow/app/src/main/java/br/com/sailboat/mobile/labs/android/kotlin/flow/MainViewModel.kt
@@ -3,15 +3,13 @@ package br.com.sailboat.mobile.labs.android.kotlin.flow
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 class MainViewModel: ViewModel() {
 
     init {
-        collectFlow()
+        collectFlowWithFoldTerminalOperator()
     }
 
     // Hot Flow: Emit values even when there no collectors
@@ -40,6 +38,60 @@ class MainViewModel: ViewModel() {
                 delay(1500L)
                 println("The current time is $time")
             }
+        }
+    }
+
+    private fun collectFlowWithOperators() {
+        viewModelScope.launch {
+            countDownFlow.filter { time ->
+                time % 2 == 0
+            }.map { time ->
+                   time * time
+            }.onEach { time ->
+                println(time)
+            }.collect { time -> // collect just make sure that the flow is finished
+                println("The current time is $time")
+            }
+        }
+    }
+
+    private fun collectFlowWithCountTerminalOperator() {
+        viewModelScope.launch {
+            val count = countDownFlow.filter { time ->
+                time % 2 == 0
+            }.map { time ->
+                time * time
+            }.onEach { time ->
+                println(time)
+            }.count {  // count the values that matches specific conditions
+                it % 2 == 0
+            }
+
+            println("The count is $count")
+        }
+    }
+
+    private fun collectFlowWithReduceTerminalOperator() {
+        viewModelScope.launch {
+            val reduceResult = countDownFlow.reduce { accumulator, value ->  // reduce will now be executed for every single emition
+                // accumulator: in the first iteration, accumulator is initiated with the first value: 10. From the second iteration,
+                // accumulator will have the integer returned on reduce filter
+                // value: value is always initiated from the second value: 9, to 8, 7, 6, 5, 4, 3, 2, 1 and 0
+                println("reduce accumulator: $accumulator, reduce value: $value")
+                accumulator + value
+            }
+            println("reduce result: $reduceResult")
+        }
+    }
+
+    private fun collectFlowWithFoldTerminalOperator() {
+        viewModelScope.launch {
+            // same as reduce but with a initial accumulator value
+            val foldResult = countDownFlow.fold(initial = 100) { accumulator, value ->
+                println("fold accumulator: $accumulator, fold value: $value")
+                accumulator + value
+            }
+            println("fold result: $foldResult")
         }
     }
 }
