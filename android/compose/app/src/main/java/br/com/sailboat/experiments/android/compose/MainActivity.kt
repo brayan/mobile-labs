f3da8e3ab0d5cc5abcbd83dbff9d08d729560d0b
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2022-07-17 12:03:06 -0300

    Add some modifiers, alignments and arrangements

--- android/compose/app/src/main/java/br/com/sailboat/experiments/android/compose/MainActivity.kt
+++ android/compose/app/src/main/java/br/com/sailboat/experiments/android/compose/MainActivity.kt
@@ -3,29 +3,35 @@ package br.com.sailboat.experiments.android.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import br.com.sailboat.experiments.android.compose.ui.theme.ComposeTheme
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTheme {
-                Greeting("Brayann")
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentSize()
+                    .background(Color.Green),
+                verticalAlignment = Alignment.Top,
+                horizontalArrangement = Arrangement.Start,
+            ) {
+                Text("My text 1")
+                Text("My text 2")
+                Text("My text 3")
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Greeting("Brayannn")
 }
