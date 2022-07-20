c94543eece7a096c82ed26221f07e5087c3dc53b
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2022-07-20 11:13:08 -0300

    Experimenting with sequencial modifiers

--- android/compose/app/src/main/java/br/com/sailboat/experiments/android/compose/MainActivity.kt
+++ android/compose/app/src/main/java/br/com/sailboat/experiments/android/compose/MainActivity.kt
@@ -4,29 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
+            Column(
                 modifier = Modifier
+                    .background(Color.Green)
+                    .fillMaxHeight(0.5f)
                     .fillMaxWidth()
-                    .wrapContentSize()
-                    .background(Color.Green),
-                verticalAlignment = Alignment.Top,
-                horizontalArrangement = Arrangement.Start,
+                    .border(5.dp, Color.Magenta)
+                    .padding(16.dp)
+                    .border(5.dp, Color.Blue)
+                    .padding(16.dp)
             ) {
                 Text("My text 1")
                 Text("My text 2")
