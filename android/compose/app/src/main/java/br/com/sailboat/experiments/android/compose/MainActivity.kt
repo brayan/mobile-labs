aee5acffd63f716b6cd63f8d2903832aaa1f0e58
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2022-07-22 16:34:43 -0300

    Add modifiers

--- android/compose/app/src/main/java/br/com/sailboat/experiments/android/compose/MainActivity.kt
+++ android/compose/app/src/main/java/br/com/sailboat/experiments/android/compose/MainActivity.kt
@@ -5,12 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
@@ -34,8 +38,18 @@ class MainActivity : ComponentActivity() {
                     .border(5.dp, Color.Blue)
                     .padding(16.dp)
             ) {
-                Text("My text 1")
+                Text(
+                    "My text 1",
+                    modifier = Modifier
+                        .border(5.dp, Color.Yellow)
+                        .padding(5.dp)
+                        .offset(20.dp, 20.dp)
+                        .border(10.dp, Color.Blue)
+                        .padding(10.dp)
+                        .clickable {  }
+                )
                 Text("My text 2")
+                Spacer(modifier = Modifier.height(50.dp))
                 Text("My text 3")
             }
         }
