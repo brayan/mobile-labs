4a199e853d31013626cc9d73b9653f0848523dae
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2022-08-08 21:20:32 -0300

    Experimenting with TextField

--- android/compose/app/src/main/java/br/com/sailboat/experiments/android/compose/MainActivity.kt
+++ android/compose/app/src/main/java/br/com/sailboat/experiments/android/compose/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,12 +17,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.Card
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -43,6 +46,8 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlin.random.Random
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
@@ -59,26 +64,82 @@ class MainActivity : ComponentActivity() {
                     .border(5.dp, Color.Blue)
                     .padding(16.dp)
             ) {
-                Text(
-                    "My text 1",
-                    modifier = Modifier
-                        .border(5.dp, Color.Yellow)
-                        .padding(5.dp)
-                        .offset(20.dp, 20.dp)
-                        .border(10.dp, Color.Blue)
-                        .padding(10.dp)
-                        .clickable { }
-                )
-                Text("My text 2")
-                Spacer(modifier = Modifier.height(50.dp))
-                Text("My text 3")
-                Spacer(modifier = Modifier.height(50.dp))
-                val painter = painterResource(id = R.drawable.animal)
-                val description = "Animal, the Muppet!"
-                val title = "Animal, the Muppet!"
-                ImageCard(painter = painter, contentDescription = description, title = title)
-                Spacer(modifier = Modifier.height(50.dp))
-                StyledText()
+//                Text(
+//                    "My text 1",
+//                    modifier = Modifier
+//                        .border(5.dp, Color.Yellow)
+//                        .padding(5.dp)
+//                        .offset(20.dp, 20.dp)
+//                        .border(10.dp, Color.Blue)
+//                        .padding(10.dp)
+//                        .clickable { }
+//                )
+//                Text("My text 2")
+//                Spacer(modifier = Modifier.height(50.dp))
+//                Text("My text 3")
+//                Spacer(modifier = Modifier.height(50.dp))
+//                val painter = painterResource(id = R.drawable.animal)
+//                val description = "Animal, the Muppet!"
+//                val title = "Animal, the Muppet!"
+//                ImageCard(painter = painter, contentDescription = description, title = title)
+//                Spacer(modifier = Modifier.height(50.dp))
+//                StyledText()
+
+
+//                val color = remember {
+//                    mutableStateOf(Color.Yellow)
+//                }
+//                ColorBox(
+//                    modifier = Modifier
+//                        .weight(1f)
+//                        .fillMaxSize()
+//                ) {
+//                    color.value = it
+//                }
+//                Box(
+//                    modifier = Modifier
+//                        .background(color.value)
+//                        .weight(1f)
+//                        .fillMaxSize()
+//                )
+                val scaffoldState = rememberScaffoldState()
+                var textFieldState by remember {
+                    mutableStateOf("")
+                }
+                val scope = rememberCoroutineScope()
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    scaffoldState = scaffoldState,
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(horizontal = 30.dp)
+                    ) {
+                        TextField(
+                            value = textFieldState,
+                            label = {
+                                Text("Enter your name")
+                            },
+                            onValueChange = {
+                                textFieldState = it
+                            },
+                            singleLine = true,
+                            modifier = Modifier.fillMaxWidth(),
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Button(onClick = {
+                            scope.launch {
+                                scaffoldState.snackbarHostState.showSnackbar("Hello $textFieldState")
+                            }
+                        }) {
+                            Text(text = "Pls greet me")
+                        }
+                    }
+                }
             }
         }
     }
@@ -172,3 +233,27 @@ fun StyledText() {
         )
     }
 }
+
+@Composable
+fun ColorBox(
+    modifier: Modifier = Modifier,
+    updateColor: (color: Color) -> Unit,
+) {
+    Box(
+        modifier = modifier
+            .background(Color.Red)
+            .clickable {
+                updateColor(
+                    Color(
+                        red = Random.nextFloat(),
+                        green = Random.nextFloat(),
+                        blue = Random.nextFloat(),
+                        alpha = 1f,
+                    )
+                )
+            }
+    ) {
+    }
+}
+
+
