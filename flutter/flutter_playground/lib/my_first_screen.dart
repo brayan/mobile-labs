0434df8d3917dfd869a367b7089f81a9e78c824e
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2023-07-07 18:18:51 -0300

    Add flutter project

---
+++ flutter/flutter_playground/lib/my_first_screen.dart
@@ -0,0 +1,49 @@
+
+// Column(
+// mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+// crossAxisAlignment: CrossAxisAlignment.center,
+// children: [
+// Stack(
+// alignment: AlignmentDirectional.center,
+// children: [
+// Container(color: Colors.red, width: 100, height: 100),
+// Container(color: Colors.blue, width: 50, height: 50),
+// ],
+// ),
+// Stack(
+// alignment: AlignmentDirectional.center,
+// children: [
+// Container(color: Colors.blue, width: 100, height: 100),
+// Container(color: Colors.red, width: 50, height: 50),
+// ],
+// ),
+// Row(
+// mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+// crossAxisAlignment: CrossAxisAlignment.center,
+// children: [
+// Container(color: Colors.cyan, width: 50, height: 50),
+// Container(color: Colors.pinkAccent, width: 50, height: 50),
+// Container(color: Colors.purple, width: 50, height: 50),
+// ],
+// ),
+// Container(
+// color: Colors.amber,
+// height: 30,
+// width: 300,
+// child: const Text(
+// "Yellow Diamond",
+// style: TextStyle(
+// color: Colors.black,
+// fontSize: 28,
+// ),
+// textAlign: TextAlign.center,
+// ),
+// ),
+// ElevatedButton(
+// onPressed: () {
+// print("You pressed the button");
+// },
+// child: const Text("Press the button!"),
+// ),
+// ],
+// )
